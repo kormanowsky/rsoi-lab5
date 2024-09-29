2d9e956aebe30fd7dc1f7ce538a67c75eb456639
Михаил Кормановский <kormanowsky@gmail.com>
2024-09-29 12:11:58 +0300

    спрятать пуш в yc за флагом

--- services/publish-service-image.sh
+++ services/publish-service-image.sh
@@ -1,4 +1,8 @@
-full_image_name=cr.yandex.ru/${CR_REGISTRY}/${CR_REPOSITORY}-${SERVICE_NAME}-service:${IMAGE_TAG}
+if [[ -n "$USE_YANDEX_CLOUD" ]]; then
+    full_image_name=cr.yandex.ru/${CR_REGISTRY}/${CR_REPOSITORY}-${SERVICE_NAME}-service:${IMAGE_TAG}
 
-docker build -t ${full_image_name} -f ./Dockerfile ./${SERVICE_NAME}
-docker push ${full_image_name}
+    docker build -t ${full_image_name} -f ./Dockerfile ./${SERVICE_NAME}
+    docker push ${full_image_name}
+else
+    echo "YC build and push skipped"
+fi
