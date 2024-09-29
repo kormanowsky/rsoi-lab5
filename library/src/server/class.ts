721d3ecf03be5d2cc3f8884968ffceec5088f815
Михаил Кормановский <kormanowsky@gmail.com>
2024-09-29 12:42:54 +0300

    добавить Library

---
+++ library/src/server/class.ts
@@ -0,0 +1,29 @@
+import express, { Express } from 'express';
+
+export abstract class Server {
+    constructor(port: number) {
+        this.server = express();
+        this.server.use(express.json());
+
+        this.port = port;
+
+        this.initRoutes();
+    }
+
+    start(): void {
+        this.server.listen(this.port, '0.0.0.0');
+    }
+
+    stop(): void {
+        
+    }
+
+    protected getServer(): Express {
+        return this.server;
+    }
+
+    protected abstract initRoutes(): void;
+
+    private server: Express;
+    private port: number;
+}
