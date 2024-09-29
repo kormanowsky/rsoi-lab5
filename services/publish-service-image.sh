04352fe897bc52974885008d7ed11d33d4090d8f
Михаил Кормановский <kormanowsky@gmail.com>
2024-09-29 12:29:02 +0300

    удалить ненужный publish-service-image

--- services/publish-service-image.sh
+++
@@ -1,8 +0,0 @@
-if [[ -n "$USE_YANDEX_CLOUD" ]]; then
-    full_image_name=cr.yandex.ru/${CR_REGISTRY}/${CR_REPOSITORY}-${SERVICE_NAME}-service:${IMAGE_TAG}
-
-    docker build -t ${full_image_name} -f ./Dockerfile ./${SERVICE_NAME}
-    docker push ${full_image_name}
-else
-    echo "YC build and push skipped"
-fi
