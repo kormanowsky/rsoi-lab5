f85e4fa3fbc4d3e809887bfd5830f791ebdcec11
Михаил Кормановский <kormanowsky@gmail.com>
2024-10-20 20:03:02 +0300

    исправить ошибку в RENTAL_API_URL

--- services/gateway/src/run.ts
+++ services/gateway/src/run.ts
@@ -5,7 +5,7 @@ const
     port = parseInt(process.env.PORT ?? '8000', 10),
     carsApiUrl = process.env.CARS_API_URL!,
     paymentApiUrl = process.env.PAYMENT_API_URL!,
-    rentalApiUrl = process.env.RENTAL_SERVICE_URL!,
+    rentalApiUrl = process.env.RENTAL_API_URL!,
     carsClient = new CarsClient(carsApiUrl),
     server = new GatewayServer(carsClient, port);
 
