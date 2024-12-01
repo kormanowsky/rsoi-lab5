09a6cd3ee72faa8e93b828fd8bc6efa4e45f3e77
Mikhail Kormanovskii <kormanowsky@yandex-team.ru>
2024-12-02 01:13:40 +0300

    подложить auth middleware во все сервисы

--- services/cars/src/run.ts
+++ services/cars/src/run.ts
@@ -1,3 +1,5 @@
+import { AuthUsernameHeaderMiddleware, AuthKeycloakMiddleware } from '@rsoi-lab2/library';
+
 import { CarsLogic } from './logic';
 import { PostgresCarMapper, PostgresCarsStorage } from './postgres-storage';
 import { CarsServer } from './server';
@@ -5,9 +7,21 @@ import { CarsServer } from './server';
 const 
     port = parseInt(process.env.PORT ?? '8000', 10),
     connString = process.env.CONN_STRING ?? '--invalid--',
+    noOauth = Boolean(process.env.NO_OAUTH);
+
+const oauthConfig = noOauth ? null : {
+    sessionSecret: process.env.SESSION_SECRET,
+    keycloakConfig: JSON.parse(process.env.KC_CONFIG ?? '{}'),
+};
+
+const authMiddleware = noOauth ? 
+    new AuthUsernameHeaderMiddleware() : 
+    new AuthKeycloakMiddleware(oauthConfig.sessionSecret, oauthConfig.keycloakConfig);
+
+const 
     mapper = new PostgresCarMapper('cars'),
     storage = new PostgresCarsStorage(connString, mapper),
     logic = new CarsLogic(storage),
-    server = new CarsServer(logic, 'api/v1/cars', port, true);
+    server = new CarsServer(logic, authMiddleware, 'api/v1/cars', port, true);
 
 server.start();
--- services/payment/src/run.ts
+++ services/payment/src/run.ts
@@ -1,3 +1,5 @@
+import { AuthUsernameHeaderMiddleware, AuthKeycloakMiddleware } from '@rsoi-lab2/library';
+
 import { PaymentsLogic } from './logic';
 import { PostgresPaymentMapper, PostgresPaymentsStorage } from './postgres-storage';
 import { PaymentServer } from './server';
@@ -5,9 +7,21 @@ import { PaymentServer } from './server';
 const 
     port = parseInt(process.env.PORT ?? '8000', 10),
     connString = process.env.CONN_STRING ?? '--invalid--',
+    noOauth = Boolean(process.env.NO_OAUTH);
+
+const oauthConfig = noOauth ? null : {
+    sessionSecret: process.env.SESSION_SECRET,
+    keycloakConfig: JSON.parse(process.env.KC_CONFIG ?? '{}'),
+};
+
+const authMiddleware = noOauth ? 
+    new AuthUsernameHeaderMiddleware() : 
+    new AuthKeycloakMiddleware(oauthConfig.sessionSecret, oauthConfig.keycloakConfig);
+
+const 
     mapper = new PostgresPaymentMapper('payment'),
     storage = new PostgresPaymentsStorage(connString, mapper),
     logic = new PaymentsLogic(storage),
-    server = new PaymentServer(logic, 'api/v1/payments', port);
+    server = new PaymentServer(logic, authMiddleware, 'api/v1/payments', port);
 
 server.start();
